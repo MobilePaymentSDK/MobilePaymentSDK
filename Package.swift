496e14a21601d359dcc17b5671cc28eefee733e2
MobilePaymentSDK <mobilesdk@payone.io>
2024-01-28 16:04:30 +0300

    SPM Update

--- Package.swift
+++ Package.swift
@@ -1,21 +1,25 @@
-// swift-tools-version:5.9
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "MobilePaymentSDK",
-    platforms: [
-        .iOS(.v12),
-    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "MobilePaymentSDK",
-            targets: ["MobilePaymentSDK"]),
+        products: [
+            .library(
+                name: "MobilePaymentSDK",
+                targets: ["MobilePaymentSDK"]
+            ),
+        ],
     ],
     targets: [
-        .target(name: "MobilePaymentSDK"),
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "MobilePaymentSDK",
-            path: "./Sources/MobilePaymentSDK.zip")
+            url: "https://github.com/MobilePaymentSDK/MobilePaymentSDK/blob/1.0.1/MobilePaymentSDK.framework.zip",
+            checksum: "7d2262e37bcbc89be8db106b36412fb313a2fdbb8996a221074e5e41d7a9e6a6"
+        ),
     ]
 )
