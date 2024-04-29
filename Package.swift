9e93ddac8a821fc2bd1b3b9c2c062339bba8d7c5
MobilePaymentSDK <mobilesdk@payone.io>
2024-04-30 00:26:57 +0300

    bug fixing

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MobilePaymentSDK",
-            url: "https://github.com/MobilePaymentSDK/MobilePaymentSDK/releases/download/1.0.7/MobilePaymentSDK.xcframework.zip",
-            checksum: "b394950bdfabf684c6c2901a3e67e56aa4da19061282fa97145001c2e7124eed"
+            url: "https://github.com/MobilePaymentSDK/MobilePaymentSDK/releases/download/1.0.8/MobilePaymentSDK.xcframework.zip",
+            checksum: "f315b080f2b559fa79448652052fcdc32851d321064e1eac505493329b34c21d"
         ),
     ]
 )
